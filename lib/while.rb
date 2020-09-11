bab7e8a337224e848992f0c779aea7c25d134996
Moose Davis <moosedavis2011@gmail.com>
2020-09-11 21:28:25 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,9 @@
 
-def using_while
+def using_until
   levitation_force = 6
-  while levitation_force < 10 
+  until levitation_force == 10 
     puts "Wingardium Leviosa"
     levitation_force += 1
-  end
 end 
 
-using_while
+using_until
